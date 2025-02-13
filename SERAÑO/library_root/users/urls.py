23423e2d3de7ce2b32ca424045977f16cb537813
NicAlbert9 <152366813+NicAlbert9@users.noreply.github.com>
2025-02-13 22:35:16 +0800

    Delete SERAÑO/library_root directory

--- SERAÑO/library_root/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.user_list, name='user_list'),
-    path('add/', views.user_create, name='user_create'),
-    path('<int:id>/edit/', views.user_update, name='user_update'),
-    path('<int:id>/delete/', views.user_delete, name='user_delete'),
-]
